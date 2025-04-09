81b4a72de069cc0c010f38a13d970d40c2435ee8
Pedro Queiroz <douglas.333.goncalves@gmail.com>
2025-04-09 02:45:20 -0300

    Advancing user authentication

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,13 +1,15 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/firestore"; // Import Firestore
+// firebaseConfig.ts (ou .js)
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyA5JnJ2SOQT3Gu7Zm1HBroBG8HOk6m-zZQ",
-    authDomain: "invoice-control-96756.firebaseapp.com",
-    projectId: "invoice-control-96756",
-    storageBucket: "invoice-control-96756.appspot.com",
-    messagingSenderId: "408770631530",
-    appId: "1:408770631530:web:ae6b88d11858aea4b83713"
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyA5JnJ2SOQT3Gu7Zm1HBroBG8HOk6m-zZQ",
+  authDomain: "invoice-control-96756.firebaseapp.com",
+  projectId: "invoice-control-96756",
+  storageBucket: "invoice-control-96756.appspot.com",
+  messagingSenderId: "408770631530",
+  appId: "1:408770631530:web:ae6b88d11858aea4b83713",
+};
 
-export const db = firebaseApp.firestore();
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
